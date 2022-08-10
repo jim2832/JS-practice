f295607416697b04620d25a694233005d4e62f54
Jim Lee <jim880705@gmail.com>
2022-08-10 23:37:52 +0800

    js practice

---
+++ number.js
@@ -0,0 +1,34 @@
+//數字及其用法
+
+document.write((6+5)*3);
+document.write("<br>");
+
+var number = -25;
+document.write(number);
+document.write("<br>");
+
+//絕對值
+document.write(Math.abs(number));
+document.write("<br>");
+
+//最大和最小值
+document.write(Math.max(10, 20, 55, 16, 100)); //100
+document.write("<br>");
+document.write(Math.min(5, 8, 100, 59, 21)); //5
+document.write("<br>");
+
+//四捨五入
+document.write(Math.round(2.6)); //3
+document.write("<br>");
+
+//次方
+document.write(Math.pow(5,3)); //125
+document.write("<br>");
+
+//根號
+document.write(Math.sqrt(121)); //11
+document.write("<br>");
+
+//隨機數
+document.write(Math.round(Math.random()*10)); //0~10的隨機數
+document.write("<br>");
--- string.js
+++ string.js
@@ -1,2 +1,31 @@
 //字串及其用法
-var phrase = "Hello World";
+
+//字串相加
+var phrase = "Hello \"World\" " + "first try ";
+var text = "second try ";
+document.write(phrase + text);
+document.write("<br>");
+
+//字串長度
+document.write(phrase.length);
+document.write("<br>");
+
+//轉大寫
+document.write(phrase.toUpperCase());
+document.write("<br>");
+
+//轉小寫
+document.write(phrase.toLowerCase());
+document.write("<br>");
+
+//回傳字串的第n位
+document.write(phrase.charAt(0));
+document.write("<br>");
+
+//回傳某個字元的index
+document.write(phrase.indexOf("f"));
+document.write("<br>");
+
+//回傳index範圍內的子字串
+document.write(phrase.substring(0,5)); //回傳Hello
+document.write("<br>");
