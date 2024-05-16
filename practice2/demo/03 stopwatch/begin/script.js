a35730cf40775fc7b8eb42d1a2d36236e53c1168
Jim <jim880705@gmail.com>
2024-05-16 16:22:34 +0800

    240516 update

--- practice2/demo/03 stopwatch/begin/script.js
+++ practice2/demo/03 stopwatch/begin/script.js
@@ -1,24 +1,30 @@
 let seconds = 0
 let m_seconds = 0
-const displayMSeconds = document.getElementById('m_seconds')
+
+// 綁定顯示時間的元素
 const displaySeconds = document.getElementById('seconds')
+const displayMSeconds = document.getElementById('m_seconds')
+
+// 綁定按鈕元素
 const buttonStart = document.getElementById('button-start')
 const buttonStop = document.getElementById('button-stop')
 const buttonReset = document.getElementById('button-reset')
+
+// 宣告一個全域變數 interval 用來存放 setInterval 的 ID
 let interval
 
-// 綁定 buttonStart 按鈕的觸發事件
+// 綁定 Start 按鈕的觸發事件
 buttonStart.onclick = () => {
   clearInterval(interval)
   interval = setInterval(timer, 10)
 }
 
-// 綁定 buttonStop 按鈕的觸發事件
+// 綁定 Stop 按鈕的觸發事件
 buttonStop.onclick = () => {
   clearInterval(interval)
 }
 
-// 綁定 buttonReset 按鈕的觸發事件
+// 綁定 Reset 按鈕的觸發事件
 buttonReset.onclick = () => {
   clearInterval(interval)
   m_seconds = 0
@@ -41,8 +47,12 @@ const timer = () => {
 
   if (Number(m_seconds) > 99) {
     console.log('seconds')
+
+    // 累加 seconds
     seconds++
     displaySeconds.innerHTML = `0${seconds}`
+
+    // 重置 m_seconds
     m_seconds = 0
     displayMSeconds.innerHTML = `0${m_seconds}`
   }
--- practice2/demo/red-yellow-green/begin/script.js
+++ practice2/demo/red-yellow-green/begin/script.js
@@ -12,14 +12,34 @@
 
 
 // 將 title 文字改成 '報告阿柴，標題已經切換！' 並將字體顏色改成藍色
-function changeTitle(){
+function clickEventHandler(){
+  // 取得 title 元素
+  let titleDiv = document.getElementById('title')
+  console.log('before: ', titleDiv.innerText)
+  let message = '報告班長，標題已經切換！'
+  titleDiv.innerText = message;
+  console.log('after: ', titleDiv.innerText)
+  titleDiv.innerHTML = `<p>${message}</p>`
+  titleDiv.style.color = 'blue'
 }
 
+let changeButton = document.getElementById('changeButton')
+changeButton.addEventListener("click", clickEventHandler);
+
+
+// 將三個方塊綁定
 const squares = document.querySelectorAll('.colorSquare')
+const output = document.getElementById('output')
 
 // 將每個元素綁定
 squares.forEach(square => {
   square.onclick = () => {
+    console.log("click")
+    output.innerText = "你選擇了 " + square.id
+
+    // 透過 id 判斷是哪一個方塊
+    _id = square.getAttribute('id')
+    output.innerHTML = `<div class='colorSquare' id='${_id}'></div>`
   }
 })
 
